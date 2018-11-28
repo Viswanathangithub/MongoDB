8ea67884328f40d9a4be411ea458f5e27649da3d
vishwaaas@gmail.com <vishwaaas@gmail.com>
2018-11-28 23:40:55 +0530

    Swagger added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -13,7 +15,12 @@ import com.example.model.ApplicationProperties;
 @ComponentScan(basePackages= {"com.example"})
 @EnableMongoRepositories(basePackages= {"com.example.repository"})
 @EnableCaching
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication extends SpringBootServletInitializer implements CommandLineRunner {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
 
 	@Autowired
 	private ApplicationProperties applicationProperties;
